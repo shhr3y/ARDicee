ac6b0fdaf9f373937581813bd9e0b349c5efccb5
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-07-03 17:20:43 +0530

    Completed

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -23,19 +23,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+////        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let moon = SCNSphere(radius: 0.2)
+//        let material = SCNMaterial()
+//
+////        material.diffuse.contents = UIColor.red
+//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+//
+////        cube.materials = [material]
+//        moon.materials = [material]
+//
+//        let node = SCNNode()
+//
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//
+////        node.geometry = cube
+//        node.geometry = moon
+//
+//        sceneView.scene.rootNode.addChildNode(node)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+            
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            // Set the scene to the view
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = AROrientationTrackingConfiguration()
+        
+        
+        
+        print("Session is Supported: \(AROrientationTrackingConfiguration.isSupported)")
+        print("World Tracking is Supported: \(ARWorldTrackingConfiguration.isSupported)")
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +75,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
